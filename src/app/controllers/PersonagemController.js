274b8d502b76ed51ddc91954f9162ad8b9a8186c
csxmaiaP <cristhian.smaia@gmail.com>
2020-05-30 19:46:33 -0300

    Adicionado api para exclusao de registro personagem.

--- src/app/controllers/PersonagemController.js
+++ src/app/controllers/PersonagemController.js
@@ -2,6 +2,15 @@ const { personagem } = require('../models')
 
 class PersonagemController {
 
+  async getAll(req, res){
+    try {
+      const data = await personagem.findAll()
+      return res.json(data)
+    } catch (err) {
+      return res.status(400).json(err)
+    }
+  }
+
   async index(req, res) {
     try{
       const data = await personagem.findAll()
@@ -18,6 +27,22 @@ class PersonagemController {
       const data = await personagem.create(dataCreate)
       
       return res.json(data)
+    } catch (err) {
+      return res.status(400).json(err)
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      // const {dataDelete} = req.body
+      // const {id} = dataDelete
+      const {id} = req.params
+
+      const data = await personagem.findByPk(id)
+
+      await data.destroy()
+
+      return res.json()
     } catch (err) {
       return res.status(400).json(err)
     }
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,13 @@ const PersonagemController = require('./app/controllers/PersonagemController')
 
 const routes = Router()
 
+routes.get('/personagem/all', PersonagemController.getAll)
+
 routes.get('/habilidade/show', HabilidadeController.index)
 routes.post('/habilidade/create', HabilidadeController.create)
 
 routes.get('/personagem/show', PersonagemController.index)
 routes.post('/personagem/create', PersonagemController.create)
+routes.delete('/personagem/delete/:id', PersonagemController.delete)
 
 module.exports = routes
