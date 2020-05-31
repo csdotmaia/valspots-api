997916c23cdc0ae0ce44e8c81dacd8a6297c234a
csxmaiaP <cristhian.smaia@gmail.com>
2020-05-31 01:08:27 -0300

    all agents added in sequelize seed.

--- src/database/seeders/20200530152143-personagem.js
+++ src/database/seeders/20200530152143-personagem.js
@@ -4,33 +4,75 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('personagem', [
       {
-        nome: 'Sova',
-        icone: "https://github.com/Blunix9/valorant-icons/blob/master/png/black/shock-bolt-black.png?raw=true",
+        nome: 'Breach',
+        icone: "https://vignette.wikia.nocookie.net/valorant/images/5/53/Breach_icon.png",
         status: 1,
         created_at: new Date(),
         updated_at: new Date()
       },
       {
         nome: 'Cypher',
-        icone: "https://github.com/Blunix9/valorant-icons/blob/master/png/black/cyber-cage-black.png?raw=true",
+        icone: "https://vignette.wikia.nocookie.net/valorant/images/8/88/Cypher_icon.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        nome: 'Omen',
+        icone: "https://vignette.wikia.nocookie.net/valorant/images/b/b0/Omen_icon.png",
         status: 1,
         created_at: new Date(),
         updated_at: new Date()
       },
       {
         nome: 'Raze',
-        icone: "https://github.com/Blunix9/valorant-icons/blob/master/png/black/blast-pack-black.png?raw=true",
+        icone: "https://vignette.wikia.nocookie.net/valorant/images/9/9c/Raze_icon.png",
         status: 1,
         created_at: new Date(),
         updated_at: new Date()
       },
       {
-        nome: 'Sage',
-        icone: "https://github.com/Blunix9/valorant-icons/blob/master/png/black/slow-orb-black.png?raw=true",
+        nome: 'Viper',
+        icone: "https://vignette.wikia.nocookie.net/valorant/images/5/5f/Viper_icon.png",
         status: 1,
         created_at: new Date(),
         updated_at: new Date()
-      }      
+      },
+      {
+        nome: 'Jett',
+        icone: "https://img.rankedboost.com/wp-content/plugins/valorant/assets/character_icons/Jett.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        nome: 'Phoenix',
+        icone: "https://img.rankedboost.com/wp-content/plugins/valorant/assets/character_icons/Phoenix.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        nome: 'Sova',
+        icone: "https://img.rankedboost.com/wp-content/plugins/valorant/assets/character_icons/Sova.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        nome: 'Brimstone',
+        icone: "https://img.rankedboost.com/wp-content/plugins/valorant/assets/character_icons/Brimstone.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },      
+      {
+        nome: 'Sage',
+        icone: "https://img.rankedboost.com/wp-content/plugins/valorant/assets/character_icons/Sage.png",
+        status: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },      
     ], {});
   },
 
