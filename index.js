e66c6a525756f686d80b87677adf507338975d97
csxmaiaP <cristhian.smaia@gmail.com>
2020-06-08 17:06:53 -0300

    cors.

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const cors = require('cors')
 
 const app = express()
 
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(cors())
 // Definição do bodyParser para a API aceitar requisições JSON
 app.use(express.json())
 
 app.use(routes)
 
+app.get('/testt', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
+})
 
-app.listen(3001)
+const server = app.listen(3001, () => {
+  console.log('Listening on port %s', server.address().port)
+})
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 const {Router} = require('express')
+const cors = require('cors')
 const HabilidadeController = require('./app/controllers/HabilidadeController')
 const PersonagemController = require('./app/controllers/PersonagemController')
 const MapaController = require('./app/controllers/MapaController')
 const SpotsController = require('./app/controllers/SpotsController')
 
 const routes = Router()
+
+const corsOptions ={
+  origin: 'http://192.168.1.4:3000/',
+  optionsSuccessStatus: 200
+}
 
 routes.get('/personagem/all', PersonagemController.getAll)
 routes.get('/mapa/all', MapaController.getAll)
