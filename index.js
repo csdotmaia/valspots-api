fae966b7a2e0d8af43086964582d49f576458211
csxmaiaP <cristhian.smaia@gmail.com>
2020-06-08 20:45:52 -0300

    ja nao sei

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const cors = require('cors')
 const app = express()
 
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: 'http://localhost',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors())
+app.use(cors(corsOptions))
 // Definição do bodyParser para a API aceitar requisições JSON
 app.use(express.json())
 
 app.use(routes)
 
-app.get('/testt', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
+app.get('/testt', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only asd.'})
 })
 
 const server = app.listen(3001, () => {
