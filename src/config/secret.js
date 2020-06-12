354da2e3735745fd43290820fd79b05df495636c
csxmaiaP <cristhian.smaia@gmail.com>
2020-06-12 14:54:58 -0300

    simple get token

--- src/config/secret.js
+++ src/config/secret.js
@@ -1,6 +1,7 @@
 const config = {}
 
 // config.JWT_KEY = 'cavalobighorse_secretkey'
+config.WEBGETSIMPLETOKEN = 'userAgentToken'
 config.TOKEN = 'cavalobighorse_secretkey\/\/'
 config.BCRYPT_KEY = 'QWE123QWEbighorse_secretkey'
 config.SALT_ROUNDS = 13
--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,21 @@ const PersonagemController = require('./app/controllers/PersonagemController')
 const MapaController = require('./app/controllers/MapaController')
 const SpotsController = require('./app/controllers/SpotsController')
 
-function checkIp(req, res, next) {
-  const {token} = req.body
-  if(token === config.TOKEN){
+function checkTokenGet(req, res, next) {
+  const {token} = req.headers
+  if(token === config.WEBGETSIMPLETOKEN){
     next()
   }else{
-    return res.status(404).json({'err': 'IP Not Allowed'})
+    return res.status(404).json({'err404': 'You dont H/ Access'})
   }
 }
 
 const routes = Router()
 
-routes.get('/personagem/all', PersonagemController.getAll)
-routes.get('/mapa/all', MapaController.getAll)
-routes.get('/habilidade/all', HabilidadeController.getAll)
-routes.get('/spot/all', SpotsController.index)
+routes.get('/personagem/all', checkTokenGet, PersonagemController.getAll)
+routes.get('/mapa/all',checkTokenGet, MapaController.getAll)
+routes.get('/habilidade/all',checkTokenGet, HabilidadeController.getAll)
+routes.get('/spot/all',checkTokenGet, SpotsController.index)
 
 // routes.post('/habilidade/create', HabilidadeController.create)
 
